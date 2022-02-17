7caf34400b3e4c738962d776409bb9b989938182
Chen-Wei Hung <luckyseven.jwh@gmail.com>
2022-02-17 16:17:45 -0600

    hide my keys

--- lib/twittermining.py
+++ lib/twittermining.py
@@ -1,13 +1,21 @@
 import tweepy
-api_key = "xLo0Ix5hPIm6qtNLaDoLbCOne"
-api_key_secret = "Q5gzA6TzKOzh4VkmFs6odu7P8Nm6LxsLT2Sd951eiQK0DUEESu"
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAANhlZQEAAAAATt9qsjhnnF9bqa%2BhbgMfPmS8iL0%3DftqpOtrWXTvEZvfsT9yU1gyrKwRunLOdeL9RiyS4uWAgzrMs24'
-access_token = "949282964352856066-Y55IFHsFizYxCq9GK4FwkjOOvdAxHaH"
-access_token_secret = "nNtPRqbBFckn5ybdfjAAUq8ppVZmdPLNNvM0f42ydkRRn"
+from pathlib import Path
+
+consumer_key_path = '../../twitter_key/comsumer_key.txt'
+consumer_secret_path = '../../twitter_key/consumer_secret.txt'
+access_token_path = '../../twitter_key/access_token.txt'
+access_token_secret_path = '../../twitter_key/access_token_secret.txt'
+bearer_token_path = '../../twitter_key/bearer_token.txt'
+
+consumer_key = Path(consumer_key_path).read_text()
+consumer_secret = Path(consumer_secret_path).read_text()
+access_token = Path(access_token_path).read_text()
+access_token_secret = Path(access_token_secret_path).read_text()
+bearer_token = Path(bearer_token_path).read_text()
 
 # Creating the authentication object
-auth = tweepy.OAuthHandler(api_key, api_key_secret)
-# Setting your access token and secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# Setting your access token and secret``
 auth.set_access_token(access_token, access_token_secret)
 # Creating the API object while passing in auth information
 api = tweepy.API(auth)
